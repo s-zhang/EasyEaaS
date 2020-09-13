71f9c1fd7ba660de63453251c94e57ed3c3bbcc6
Steven Zhang <szh10000@gmail.com>
2020-09-13 00:40:49 -0700

    Fix deploy 1

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ HOME_DIR=/home/szh10000
 TARGET_DEPLOY_DIR=$HOME_DIR/deploy/EasyEaaS
 SOURCE_DIR=./dist
 
-ssh $TARGET -i $KEY_FILE "cd $HOME_DIR/docker && (docker-compose down easyeaas || true) && (rm -rf $TARGET_DEPLOY_DIR || true) && mkdir $TARGET_DEPLOY_DIR"
+ssh $TARGET -i $KEY_FILE -t "bash -ci \"cd $HOME_DIR/docker && (docker-compose down easyeaas || true) && (mkdir $TARGET_DEPLOY_DIR || true) && (rm -rf $TARGET_DEPLOY_DIR || true) && (mkdir $TARGET_DEPLOY_DIR || true)\""
 scp -r -i $KEY_FILE $SOURCE_DIR/* $TARGET:$TARGET_DEPLOY_DIR
-ssh $TARGET -i $KEY_FILE "cd $HOME_DIR/docker && docker-compose up easyeaas"
+ssh $TARGET -i $KEY_FILE -t "bash -ci \"cd $HOME_DIR/docker && docker-compose up easyeaas\""
